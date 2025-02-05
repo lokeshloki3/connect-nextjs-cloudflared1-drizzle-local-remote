6bc835b99b49762ae4e3ea90b499bbf898e8d635
lokeshloki3 <lokesh1992p@gmail.com>
2025-02-05 23:36:57 +0530

    Trying auto cicd by cloudflare pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', // Enables static export
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
